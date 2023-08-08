760c94e06977e30de929f68a0b39779eac363021
Your Name <you@example.com>
2023-08-09 03:01:35 +0700

    f

---
+++ app/index.js
@@ -0,0 +1,22 @@
+const lyrics = require("lyric-music"); 
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+
+
+app.get('/lirik/:id',async (req, myres) => {
+    const lyric = await lyrics(req.params.id);
+    var jsonObj = {'lirik':lyric};
+
+    myres.json(jsonObj);
+  
+});
+
+
+app.listen(port, () => {
+    console.log(`cli-nodejs-api listening at http://localhost:${port}`)
+});
